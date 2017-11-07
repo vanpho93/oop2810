692ebfed365245251772a426be713349e257ac2b
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-07 18:59:47 +0700

    class

--- d.js
+++ d.js
@@ -4,15 +4,17 @@ class Person {
         this.age = age;
     }
 
-    getYearOfBirth() {
+    get yearOfBirth() {
         return 2017 - this.age;
     }
 
+    set yearOfBirth(value) {
+        this.age = 2017 - value;
+    }
     sayHello() {
         console.log('Hello, I am ' + this.name);
     }
 }
-
 class Child extends Person {
     constructor(name, age, toy) {
         super(name, age);
@@ -22,12 +24,14 @@ class Child extends Person {
         console.log('Hi, I am playing ' + this.toy);
     }
     sayHello() {
+        super.sayHello();
         console.log('Hi, I am ' + this.name);
     }
 }
-
 const teo = new Child('Teo', 10, 'Auto');
 console.log(teo);
 teo.sayHello();
 teo.sayHi();
-console.log(teo.getYearOfBirth());
+console.log(teo.yearOfBirth);
+teo.yearOfBirth = 2010;
+console.log(teo.yearOfBirth);
